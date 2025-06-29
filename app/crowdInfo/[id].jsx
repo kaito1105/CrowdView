2d948acb476bcdac31d649fee6d369d8ee4f1a34
kaito1105 <kai20011105@gmail.com>
2025-06-29 17:42:06 -0500

    Add comments section

--- app/crowdInfo/[id].jsx
+++ app/crowdInfo/[id].jsx
@@ -1,20 +1,21 @@
+import Comments from "@/components/Comments";
+import { FACILITIES } from "@/constants/Facilities";
+import UseLocation from "@/hooks/useLocation";
 import { useLocalSearchParams } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
-import { FACILITIES } from "../../constants/Facilities";
-import UseLocation from "../../hooks/useLocation";
 
 export default function CrowdInfoScreen() {
   const {id} = useLocalSearchParams();
   const facility = FACILITIES.find(f => f.id === id);
 
   return (
-    <View style={styles.container}>
+    <View style={styles.background}>
       {facility ? (
-        <View>
+        <View style={styles.container}>
           <Text style={styles.title}>{facility.name}</Text>
           <Text style={styles.crowdInfo}>Crowd level: </Text>
           <Text style={styles.vote}>Vote:</Text>
-          <Text style={styles.comment}>Comment:</Text>
+          <Comments />
           <UseLocation 
             centerCoords={facility.center} 
             edgeCoords={facility.edge}
@@ -28,8 +29,11 @@ export default function CrowdInfoScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
+    backgroundColor: "#fff",
+  },
+  container: {
     marginHorizontal: 20,
     marginVertical: 20,
   },
@@ -44,10 +48,6 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   vote: {
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  comment: {
     fontWeight: "bold",
     marginBottom: 10,
   },
--- constants/Region.js
+++ constants/Region.js
@@ -1,6 +1,6 @@
 export const INITIAL_REGION = {
-  latitude: 42.24755809691715, 
-  longitude: -87.8291705184884,
+  latitude: 42.24674149346803, 
+  longitude:  -87.82792870336029,
   latitudeDelta: 0.007,
   longitudeDelta: 0.007,
 };
