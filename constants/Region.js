3fb364fa2c91ee2cc71c157d4dc5b1ad3d37b1a3
kaito1105 <kai20011105@gmail.com>
2025-07-02 13:44:08 -0500

    Update an initial region on map

--- constants/Region.js
+++ constants/Region.js
@@ -1,6 +1,6 @@
 export const INITIAL_REGION = {
-  latitude: 42.24674149346803, 
-  longitude:  -87.82792870336029,
+  latitude: 42.247075433277324,
+  longitude: -87.82816991757883,
   latitudeDelta: 0.007,
   longitudeDelta: 0.007,
 };
